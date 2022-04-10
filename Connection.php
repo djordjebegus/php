a3e126de3d4ee168da718c4bcacea91e52b1f219
djordjebegus <51109967+djordjebegus@users.noreply.github.com>
2022-04-10 18:41:13 +0200

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,1 +1,12 @@
+class Connection {
+    public static function connect($database)
+    {
+        try{        return new PDO('mysql:host='.$database['host'].';
+            dbname='.$database['dbname'].';',$database['user'],$database['password']);
+              }
+        catch(PDOException $e){
+            die("Error".$e->getMessage());
 
+        }      
+  }
+}
